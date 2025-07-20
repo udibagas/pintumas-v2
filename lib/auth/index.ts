0202569787df869bf58bc43d60bce9ebb147015c
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-07-21 00:17:00 +0700

    update

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -10,12 +10,6 @@ export * from "../auth-edge";
 // Export all universal functions
 export * from "../auth-universal";
 
-// Export types
-export * from "../auth.types";
-
-// Export configuration
-export * from "../edge-runtime-config";
-
 // Default export for backward compatibility
 export { verifyTokenEdge } from "../auth-edge";
 export {
