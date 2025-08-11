473affcbcc20e7b41597d0024cf5bc9218033112
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-08-11 09:55:38 +0700

    add type

--- hooks/useFetch.ts
+++ hooks/useFetch.ts
@@ -1,8 +1,11 @@
 import { getAll } from "../lib/api";
 import { useQuery } from "@tanstack/react-query";
 
-export function useFetch(endpoint: string, params: Record<string, any> = {}) {
-  return useQuery({
+export function useFetch<T = any>(
+  endpoint: string,
+  params: Record<string, any> = {}
+) {
+  return useQuery<T>({
     queryKey: [endpoint, params],
     queryFn: () => getAll(endpoint, params),
     staleTime: 60 * 1000 * 10, // 10 minutes
